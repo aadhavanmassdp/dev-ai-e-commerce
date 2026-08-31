84e32423898e3c0fa82a4e2e1e2752e8eaaf4a79
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2026-08-31 21:45:51 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Currently empty, but can be used for utilities like currency formatting,
+// product rendering helpers, etc.
+// Example: format currency
+window.formatCurrency = function(amount) {
+    return '$' + amount.toFixed(2);
+};
